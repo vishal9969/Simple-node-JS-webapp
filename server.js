55c786fb10583e292a55fda99acf7fa4ad782fa3
vishal9969 <112000540+vishal9969@users.noreply.github.com>
2023-10-27 23:30:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'));
+app.get('/', (req, res) => {
+  res.send('Hello, Node.js!');
+});
 
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
